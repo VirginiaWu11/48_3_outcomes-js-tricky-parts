54e880aecda2ba2f365856a200128f6bbcb33f7f
Virginia Wu <virginiawu11@gmail.com>
2022-02-06 22:33:17 -0500

    complete exercise

--- create-account.js
+++ create-account.js
@@ -1,5 +1,44 @@
-function createAccount(pin, amount) {
+function createAccount(pin, amount = 0) {
+  let balance = amount;
+  let PIN = pin;
+  function checkPin(p) {
+    if (PIN !== p) return "Invalid PIN.";
+  }
 
+  return {
+    checkBalance: function (p) {
+      if (PIN !== p) return "Invalid PIN.";
+      return `$${balance}`;
+    },
+    deposit: function (p, amt) {
+      if (PIN !== p) return "Invalid PIN.";
+      balance += amt;
+      return `Succesfully deposited $${amt}. Current balance: $${balance}.`;
+    },
+    withdraw: function (p, amt) {
+      if (PIN !== p) {
+        return "Invalid PIN.";
+      } else if (amt > balance) {
+        return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      } else {
+        balance -= amt;
+        return `Succesfully withdrew $${amt}. Current balance: $${balance}.`;
+      }
+    },
+    changePin: function (p, newPin) {
+      if (PIN !== p) return "Invalid PIN.";
+      PIN = newPin;
+      return "PIN successfully changed!";
+    },
+  };
 }
+
+// let account = createAccount("1234", 100);
+// console.log(account.checkBalance("oops"));
+// console.log(account.checkBalance("1234"));
+// console.log(account.deposit("1234", 250));
+// console.log(account.withdraw("1234", 300));
+// console.log(account.withdraw("1234", 10));
+// console.log(account.changePin("1234", "5678"));
 
 module.exports = { createAccount };
--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,17 @@
-function curriedAdd(total) {
-
+function curriedAdd(total = 0) {
+  let currentTotal = total;
+  if (total === 0) return currentTotal;
+  return function add(num = null) {
+    if (num === null) {
+      return currentTotal;
+    } else {
+      currentTotal += num;
+      return add;
+    }
+  };
 }
+
+// let firstAdder = curriedAdd();
+// console.log(firstAdder(1)(2)());
 
 module.exports = { curriedAdd };
